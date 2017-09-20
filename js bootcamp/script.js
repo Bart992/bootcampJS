26d65de6fe2cc4465e0b29b36ea0cdba31b5c76a
Bart992 <bart-de-jong@outlook.com>
2017-09-20 08:37:47 +0200

    Add files via upload

---
+++ js bootcamp/script.js
@@ -0,0 +1,46 @@
+var string1 = "ford";
+var string2 = "renault";
+var string3 = "toyota";
+var string4 = "bmw";
+var string5 = "fiat";
+var string6 = "citroen";
+var string7 = "mercedes";
+var string8 = "volkswagen";
+var string9 = "porsche";
+var string10 = "cadilac";
+
+var nr1 = 1;
+var nr2 = 2;
+var nr3 = 3;
+var nr4 = 4;
+var nr5 = 5;
+
+var boolean1 = true;
+var boolean2 = false;
+var boolean3 = true;
+var boolean4 = false;
+var boolean5 = false;
+
+console.log(string4+nr3);
+
+console.log(nr1+boolean3);
+
+console.log(string10+boolean2);
+
+console.log(nr5+1200);
+
+console.log(1200+nr);
+
+console.log("nr4="+typeof(nr));
+
+
+
+
+//introspection
+var nr=5;
+var str="5";
+
+console.log(nr+str);
+
+console.log("string5="+typeof( str) );
+console.log("nr3="+typeof( nr) );
---
+++ js bootcamp/script2.js
@@ -0,0 +1,30 @@
+functie1();
+
+function functie1() {
+  var vn = "Bart ";
+  var an = "de Jong";
+
+  console.log(vn+an);
+}
+
+
+var functie2 = function() {
+   console.log(9*9);
+};
+
+var functie3 = function() {
+   console.log("het getal is " + 12);
+};
+
+functie2();
+functie3();
+
+var anonymous1 = function() {
+    console.log("Rowinaaa");
+}
+anonymous1()
+
+var anonymous2 = function() {
+    console.log("Reinderd");
+}
+anonymous2()
